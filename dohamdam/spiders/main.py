baa50aa2ea407edc60f44857e67220c80f1d5315
mohammad <mm.74noroozi@gmail.com>
2021-02-06 11:32:26 +0330

    fix: new address

--- dohamdam/spiders/main.py
+++ dohamdam/spiders/main.py
@@ -4,20 +4,20 @@ from dohamdam.items import DohamdamItem
 class QuotesSpider(scrapy.Spider):
     name = "dohamdam"
     start_urls = [
-        'http://2kx.ir/',
+        'http://oruv.ir/',
     ]
     completed=[]
     def parse(self, response):
         token = response.css('#sec1').attrib['value']+response.css('#sec7').attrib['value']+response.css('#sec4').attrib['value']+response.css('#sec5').attrib['value']+response.css('#sec2').attrib['value']+response.css('#sec3').attrib['value']+response.css('#sec6').attrib['value']
         yield scrapy.FormRequest.from_response(
             response
-            ,formdata={"myusername":"09057279015","mypassword":"bg2g}aSino1","St2":token},
+            ,formdata={"myusername":"a.khatami70@gmail.com","mypassword":"neverhood0111","St2":token},
             callback=self.go_to_index2
         )
 
     def go_to_index2(self, response):
-        for i in range(1):
-            yield scrapy.Request(f'http://2kx.ir/search_prof.php?sel=1&op=3&sen1=18&sen2=40&vez=1219&ostan=326&city=339&picu=1219&vsalamat=1&tah=1219&vesht=1219&ghad1=1219&ghad2=1219&vazn1=1219&vazn2=70&postrang=1219&dkhan1=1219&dkhan2=1219&dhamsar1=1219&dhamsar2=1219&vmaskan=1219&vmashin=1219&dinm=1219&eteghad=1219&vhejab=1219&min={i*12}&page=12',callback=self.page)
+        for i in range(50):
+            yield scrapy.Request(f'http://oruv.ir/search_prof.php?sel=1&op=3&sen1=1219&sen2=30&vez=1219&ostan=441&city=442&picu=1219&vsalamat=1219&tah=1219&vesht=1219&ghad1=1219&ghad2=1219&vazn1=1219&vazn2=1219&postrang=1219&dkhan1=1219&dkhan2=1219&dhamsar1=1219&dhamsar2=1219&vmaskan=1219&vmashin=1219&dinm=1219&eteghad=1219&vhejab=1219&min={i*12}&page=12',callback=self.page)
 
     def page(self,response):
         persons= response.css(".tooltip")
@@ -25,13 +25,13 @@ class QuotesSpider(scrapy.Spider):
             personId = person.attrib['href'].split("=")[1]
             if personId not in self.completed:
                 self.completed.append(personId)
-                yield scrapy.Request(f'http://2kx.ir/send_msg.php?uid={personId}&op=1',callback=self.send_msg, meta={'personId': personId})
+                yield scrapy.Request(f'http://oruv.ir/send_msg.php?uid={personId}&op=2',callback=self.send_msg, meta={'personId': personId})
     def send_msg(self,response):
         personId = response.meta.get('personId')
         token = response.css('#sec1').attrib['value']+response.css('#sec7').attrib['value']+response.css('#sec4').attrib['value']+response.css('#sec5').attrib['value']+response.css('#sec2').attrib['value']+response.css('#sec3').attrib['value']+response.css('#sec6').attrib['value']
         yield scrapy.FormRequest.from_response(
             response
-            ,formdata={"security_token2":token},
+            ,formdata={"security_token2":token,'title':'سلام وقت بخیر','message':"قصد اشنایی با شما رو دارم. مایلید با هم بیشتر صحبت کنیم؟"},
             callback=self.save,
             meta={'personId': personId}
         )
@@ -39,5 +39,4 @@ class QuotesSpider(scrapy.Spider):
         personId = response.meta.get('personId')
         item = DohamdamItem()
         item['id']= personId
-        item['html']=response.text
         yield item
